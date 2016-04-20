ba532b7e4eeea85db7794d56dd715b85cc993325
Thierry Nouidui <tsnouidui@lbl.gov>
2016-04-20 16:12:28 -0700

    Fix linkBCVTB.

--- bin/linkBCVTB.sh
+++ bin/linkBCVTB.sh
@@ -3,7 +3,7 @@
 #echo Remove any existing symbolic link for /usr/local/lib/libbcvtb.dylib
 # The symbolic link will be set only if /usr/local/lib exists which
 # seems to be true on Mac OS X 10.11 but not on earlier versions of Mac OS X.
-if [ -d /usr/local/lib/ ]; then
+if [[ -w /usr/local/lib/ && -w /usr/local/lib/ ]] ; then
   rm -rf /usr/local/lib/libbcvtb.dylib
   #echo Create new symbolic link for /usr/local/lib/libbcvtb.dylib
   ln -s $1"/lib/util/libbcvtb.dylib" "/usr/local/lib/libbcvtb.dylib"
